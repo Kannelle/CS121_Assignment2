967f468f0bba16c7e62fc02f86ebb71f5c600fd0
Kannelle <kristinawong54@gmail.com>
2016-01-27 21:46:17 -0800

    Time to Crawl Domain
    Answered question 1 of the assignment,
    "How much time did it take to crawl the entire domain?" by creating a
    method that prints the output.

--- src/ir/assignments/three/Controller.java
+++ src/ir/assignments/three/Controller.java
@@ -22,11 +22,8 @@ public class Controller {
         config.setPolitenessDelay(600);
 
         // Set maximum crawl depth
-<<<<<<< HEAD
-        config.setMaxDepthOfCrawling(-1);
-=======
         config.setMaxDepthOfCrawling(1);
->>>>>>> origin/master
+
         
         // Set the maximum number of pages to crawl
         config.setMaxPagesToFetch(8000);
@@ -44,26 +41,48 @@ public class Controller {
          * URLs that are fetched and then the crawler starts following links
          * which are found in these pages
          */
-        controller.addSeed("http://www.ics.uci.edu/~lopes/");
-        controller.addSeed("http://www.ics.uci.edu/~welling/");
         controller.addSeed("http://www.ics.uci.edu/");
 
+        // Start the timer for crawlTime 
+        long start = System.currentTimeMillis( );
         /*
          * Start the crawl. This is a blocking operation, meaning that your code
          * will reach the line after this only when crawling is finished.
          */
         controller.start(Crawler.class, numberOfCrawlers);
+        
+        // Find out how much time it took to crawl the entire domain
+        crawlTime(start);
     }
 	
-	// Method to find out how much time it took to crawl the entire domain	
+	// Method to find out how much time it took to crawl the entire domain and print it
+	public static void crawlTime (long startTime) {
+		long endTime = System.currentTimeMillis( );
+		long elapsedTime = endTime - startTime;
+		System.out.println("Time elapsed: "+elapsedTime+" ms");
+	}
 	
 	// Method to determine how many unique pages found in the entire domain
+	// (Uniqueness is established by the URL, not the page's content.)
 	
-	// Method to find how many unique pages found in the entire domain
+	// Method to find the number of subdomains found.
+	// Submit the list of subdomains ordered alphabetically and the number 
+	// of unique pages detected in each subdomain. 
+	// The file should be called Subdomains.txt, and its content should be 
+	// lines containing the URL, a comma, a space, and the number.
 	
-	// Method to find the longest page in terms of number of words
+	// Method to find the longest page in terms of number of words.
+	// (Don't count HTML markup as words.)
 	
-	// Method to find the 500 most common words in this domain
+	// Method to find the 500 most common words in this domain.
+	// (Ignore English stop words, which can be found, for example, 
+	// at http://www.ranks.nl/stopwords.) 
+	// Submit the list of common words ordered by frequency 
+	// (and alphabetically for words with the same frequency) 
+	// in a file called CommonWords.txt.
+	
+
+
 	
 	
 
